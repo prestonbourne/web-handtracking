e8c0bccc14eb8dc96a9fcb074f76d8d3ce898b1d
saintpreston <90055250+Saintpreston@users.noreply.github.com>
2023-04-17 21:03:51 -0400

    remove serial comm

--- server/main.js
+++ server/main.js
@@ -1,59 +1,28 @@
-const { SerialPort } = require("serialport");
 const express = require("express");
 const bodyParser = require("body-parser");
-const cors = require('cors');
-
-const routes = require("./routes");
-const { state, LIGHT } = require("./state");
-
-
-const app = express();
-const port = 3001;
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static("public"));
-app.use(express.json());
-app.use(cors())
-app.use("/", routes);
-
-app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
-});
+const cors = require("cors");
+const routes = require("./routes.js");
+const dotenv = require('dotenv');
 
 (function main() {
-  const path = "/dev/cu.usbmodem14201";
-  const baudRate = 9600;
-  const port = new SerialPort({ path, baudRate });
-
-  port.on("open", () => {
-    console.log("Serial Port succesfully opened", "baudRate: " + port.baudRate);
-    
-
-    function sendData(){
-      if (state.lightStatus) {
-      
-        port.write(Number(1).toString())
-      }
-      else{
-       
-        port.write(Number(0).toString())
-      }
-
-    }
+  dotenv.config();
+  const app = createExpressApp();
 
 
-
-    setInterval(sendData, 250);
-
-
-});
-
-
-  
-
-
-
-  port.on("close", () => {
-    clearInterval(intervalId);
-  });
 })();
+
+function createExpressApp() {
+  const port = process.env.PORT || 3001;
+  const app = express();
+
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(express.static("public"));
+  app.use(express.json());
+  app.use(cors());
+  app.use("/", routes);
+
+  app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+  });
+  return app;
+}
--- server/routes.js
+++ server/routes.js
@@ -3,20 +3,23 @@ const {state, LIGHT} = require("./state")
 
 const router = express.Router();
 
+
+
 router.get('/', (req, res) => {
 
   res.json({
-   message: ""
+   message: "Hello from Preston's Heroku Server"
   })
   
 });
 
 router.post('/', (req, res) => {
-  // const newTodo = { name: req.body.name, completed: false };
-  // todos.push(newTodo);
+
 
  const newStatus = req.body.status;
- console.log(req)
+ 
+
+
 
 if(newStatus === "true"){
   state.lightStatus = LIGHT.ON
