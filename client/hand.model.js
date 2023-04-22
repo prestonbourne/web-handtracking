301adb8932d916c5a4c4bf197d4edaf72b0e72dc
saintpreston <90055250+Saintpreston@users.noreply.github.com>
2023-04-21 23:34:51 -0400

    abstract hand rendering logic into class

---
+++ client/hand.model.js
@@ -0,0 +1,32 @@
+import { UPDATE_LANDMARKS_EVENT } from "./constants";
+
+/**
+ * @typedef Landmark
+ * @type {object}
+ * @property {number} x
+ * @property {number} y
+ * @property {number} z
+ */
+
+
+class HandModel {
+
+  constructor() {
+    this.landmarks = [];
+  }
+
+
+
+  /**
+   * @param {Landmark[]} landmarks
+   */
+  setLandmarks(landmarks) {
+    this.landmarks = landmarks;
+    window.dispatchEvent(new Event(UPDATE_LANDMARKS_EVENT));
+  }
+
+}
+
+const model = new HandModel();
+
+export default model;
