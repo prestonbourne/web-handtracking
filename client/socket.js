cb70dcb94d10a7597f12052534f8f475a8495411
saintpreston <90055250+Saintpreston@users.noreply.github.com>
2023-05-07 23:50:34 -0400

    add `reconnectionAttempts` to param

--- client/socket.js
+++ client/socket.js
@@ -3,7 +3,9 @@ import { io } from "socket.io-client";
 
 const SERVER_BASE_URL = import.meta.env.VITE_SERVER_BASE_URL
 
-const socket = io(SERVER_BASE_URL);
+const socket = io(SERVER_BASE_URL, {
+ reconnectionAttempts: 3
+});
 
 
 
